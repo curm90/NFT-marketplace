1fb33def25294b9f4ddbf63fdfd932b53352dd03
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-17 17:16:20 +0100

    add header component

---
+++ src/app/Header/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <nav className='flex items-center justify-between gap-4 px-6 py-3'>
+      <div className='flex gap-6'>
+        <span>Logo here</span>
+        <span>Home</span>
+      </div>
+    </nav>
+  );
+}
