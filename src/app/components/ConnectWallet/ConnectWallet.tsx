a09d7fd54a4cb39528295e0f836ee5e13f0f7393
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-17 19:41:19 +0100

    add connect wallet button

---
+++ src/app/components/ConnectWallet/ConnectWallet.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import client from '@/app/utils/thirdwebClient';
+import { ConnectButton } from 'thirdweb/react';
+import { createWallet, inAppWallet } from 'thirdweb/wallets';
+
+const wallets = [inAppWallet(), createWallet('io.metamask'), createWallet('io.rabby')];
+
+export default function ConnectWallet() {
+  return (
+    <div>
+      <ConnectButton client={client} wallets={wallets} />
+    </div>
+  );
+}
