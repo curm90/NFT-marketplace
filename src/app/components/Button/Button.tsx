7d79c41faa0b2067e4f6918f35f14023ee3facef
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-18 16:16:35 +0100

    add button component

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/app/utils/classNames';
+
+type ButtonProps = {
+  type: 'submit' | 'reset' | 'button';
+  className?: string;
+  children?: React.ReactNode;
+};
+
+export default function Button({ type, className, children }: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={cn(
+        'w-fit rounded-lg bg-cyan-800 px-8 py-1 text-slate-50 duration-150 hover:bg-cyan-700',
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+}
